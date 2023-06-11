bcff4763773c704e2884a09ca1b99e0d7b26f7cc
ivanRamiro05 <ivan2224654@correo.uis.edu.co>
2023-06-11 16:03:06 -0500

    Preguntas faciles

--- modelo/Facil.java
+++ modelo/Facil.java
@@ -8,6 +8,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Facil extends Preguntas{
+    static String TextoBaseDeDatos=
+    "¿ Dondé esta ubicada la casa blanca ?\tWashington D.C.\tNew York\tCalifornia\tFlorida\tTexas"
+    +"\n"+
+    "Cuando acabo la segunda guerra mundial ?\t1945\t1944\t1943\t1942\t1941"
+    +"\n"
+    +"Quien invento la bombilla ?\tThomas Edison\tNikola Tesla\tBenjamin Franklin\tAlbert Einstein\tIsaac Newton"
+    +"\n"
+    +"Quien invento el telefono ?\tAlexander Graham Bell\tThomas Edison\tNikola Tesla\tBenjamin Franklin\tAlbert Einstein"
+    +"\n"
+    +"Cual es el pais mas grande del mundo ?\tRusia\tChina\tEstados Unidos\tCanada\tBrasil"
+    +"\n"
+    +"Cual es el rio mas caudaloso del planetas ?\tAmazonas\tNilo\tMisisipi\tGanges\tYangtze"
+    +"\n"
+    +"Cual es el pais mas visitado del mundo ?\tFrancia\tEstados Unidos\tEspaña\tChina\tItalia"
+    +"\n"
+    +"Cual es la moneda que maneja estados unidos ?\tDolar\tEuro\tPeso\tYen\tLibra"
+    +"\n"
+    +"Cual es el pais mas poblado del mundo ?\tChina\tIndia\tEstados Unidos\tIndonesia\tBrasil";
 
+    
 }
 
