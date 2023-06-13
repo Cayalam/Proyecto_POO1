b755bd62c020f74371f1dd9ed9c26cd736926e2a
Cayalam <Carlos2224648@correo.uis.edu.co>
2023-06-13 16:48:54 -0500

    se arreglo la parte de intermedio

--- vista/Intermedio.java
+++ vista/Intermedio.java
@@ -13,9 +13,9 @@ public class Intermedio extends JFrame
  + "\n"
 +"¿Cuál es la moneda oficial de Japón?\tYen\tDólar\tEuro\tPeso"
  + "\n"
-+ "¿Quién pintó: La noche estrellada?\tVincent van Gogh\tPablo Picasso\tSalvador Dalí\tjose" 
++ "¿Quién pintó: La noche estrellada?\tVincent van Gogh\tPablo Picasso\tSalvadoDalí\tjose" 
 + "\n"
-+ "¿Cuál es el océano más pequeño del mundo?\tOcéano Ártico\tOcéano Atlántico\tOcéano Índico\t" 
++ "¿Cuál es el océano más pequeño del mundo?\tOcéano Ártico\tOcéano Atlántico\tOcéano Índico\tOcéano Pacífico" 
 + "\n"
 + "¿Cuál es la montaña más alta de América del Norte?\tMonte McKinley (Denali)\tMontaña de la Mesa\tMonte Rainier\tMonte Whitney" 
 + "\n"
@@ -23,17 +23,22 @@ public class Intermedio extends JFrame
 + "\n"
 +"¿Cuál es el país con la mayor extensión territorial en América del Sur?\tBrasil\tArgentina\tPerú\tChile"
 + "\n"
-+"¿Cuál es el escritor de la famosa novela: Cien años de soledad?\tGabriel García Márquez\tMario Vargas Llosa\tJulio Cortázar\tJorge Luis Borges"
++"¿Cuál es el escritor de la famosa novela: Cien años de soledad?\tGabriel García Márquez\tMario Vargas Llosa\tJulio Cortázar\tJorge Luis Borges\tCamilo Ponce"
  + "\n"
-+"¿Cuál es el país que tiene el sistema de gobierno conocido como: monarquía parlamentaria?\tReino Unido\tEstados Unidos\tAlemania" 
++"¿Cuál es el país más grande del mundo?\tRusia\tChina\tEstados Unidos\tCanadá\tBrasil"
+ + "\n"
+
++"¿Cuál es el río más caudaloso del planeta?\tAmazonas\tNilo\tMisisipi\tGanges\tYangtsé"
+ + "\n"
+
++"¿Cuál es el símbolo químico del carbono?\tC\tCa\tCo\tCu"
+ 
 + "\n"
-+"¿Quién fue el presidente de los Estados Unidos durante la Segunda Guerra Mundial?\tFranklin D. Roosevelt\tHarry S. Truman\tDwight D. Eisenhower" 
++"¿Cuál es el país más pequeño del mundo en términos de superficie terrestre?\tCiudad del Vaticano\tMónaco\tNauru\tSan Marino"
+
 + "\n"
-+"¿Cuál es el símbolo químico del carbono?\tC\tCa\tCo" 
-+ "\n"
-+"¿Cuál es el país más pequeño del mundo en términos de superficie terrestre?\tCiudad del Vaticano\tMónaco\tNauru"
-+ "\n"
-+"¿En qué año se celebró la primera Copa Mundial de la FIFA?\t1930\t1950\t1960"
++"¿En qué año se celebró la primera Copa Mundial de la FIFA?\t1930\t1950\t1960\t1970"
+
 ;
 
 static String[] renglones = textoBaseDePreguntas.split("\n");
